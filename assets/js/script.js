dab63d12856aca102152882ff377225b2ab8a502
Mia Borja <apk.borja@gmail.com>
2022-03-24 10:41:32 -0700

    Updated localStorage variable pulling.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,11 @@ $("#currentDay").text(todayDate.format("[The current date is] dddd, MMMM Do[.]")
 
 function entryColorCode() {
     var todayHour = moment().hour();
-    var assignHour = informationEntry.attr("data-store");
 
     console.log(todayHour);
-    console.log(assignHour);
 
-    $(".time-block").children("textarea").each(function () {
+    $(".description").each(function () {
+        var assignHour = $(this).attr("data-store");
             if(todayHour < assignHour) {
                 $(this).addClass("future");
             } else if(todayHour == assignHour) {
@@ -18,21 +17,20 @@ function entryColorCode() {
             } else {
                 $(this).addClass("past");
             };
-        getValues();
     });
 };
 
-function getValues(hour) {
-    var savedDescription = localStorage.getItem(hour);
-    
-
-
-    console.log(savedDescription)
+function getValues() {
+    $(".description").each(function() {
+        for (i = 0; i < localStorage.length; i++) {
+            $(this).val(localStorage.getItem(i));
+        };
+    });
 };
 
 $(".saveBtn").on("click", function() {
     var loggedDescription = $(this).siblings(".description").val();
-    var descriptionNo = $(this).siblings(".description").data("store");
+    var descriptionNo = $(this).siblings(".description").attr("id");
     console.log($(this).siblings(".description").val());
     
     localStorage.setItem(descriptionNo, loggedDescription);
